5a5e472c2432d1aeba2500377415b2d16821d897
PRITELLI <valerieablucas@gmail.com>
2021-01-06 16:14:14 -0600

    type in characters

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// Special characters 
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 
-  passwordText.value = password;
+// Numeric characters
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-}
+// Alphabetical characters
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
