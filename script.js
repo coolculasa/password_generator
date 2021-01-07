496e398d5d92e0fc3dbb409395162dde565c5fec
PRITELLI <valerieablucas@gmail.com>
2021-01-06 21:54:57 -0600

    added on mouse click execute function and input validation

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 /* user input for criteria  */
-var inputPswd;
+var inputPswdChars;
 var includeNum;
 var incluespChars;
 var includeUpper;
 var includeLower;
+
 
 /* List of Characters */
 
@@ -17,27 +18,42 @@ num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 /* Alphabetical characters */
 alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+var generate = document.querySelector("#generate");
+
+// on mouse click run function so that password will be displayed in text area
+generate.addEventListener("click", function () {
+    document.getElementById("password").placeholder = generatePassword();
+});
 
 function generatePassword() {
   // ask number of characters from user and store in a variable 
-  inputPswd = parseInt(prompt("Choose between 8 and 128 characters for the password"));
+  inputPswdChars = parseInt(prompt("Choose between 8 and 128 characters for the password"));
 
   // validate input
   
   //if no value
-  if (!inputPswd) {
-      alert("This needs a value");
-  } else if (inputPswd < 8 || inputPswd > 128) {
+  if (!inputPswdChars) {
+      alert("This needs a value. Please click Generate Password again.");
+  } 
+  
+  if (inputPswdChars < 8 || inputPswdChars > 128) {
       
-      inputPswd = parseInt(prompt("Not within the range. Choose between 8 and 128 for the password"));
-
-  } else {
+      inputPswdChars = parseInt(prompt("Not within the range. Choose between 8 and 128 characters for the password"));
+  }
+  
+  else {
       // prompt for other criteria
       includeNum = confirm("Will the password include numbers?");
       includespChars = confirm("Will this include special characters?");
       includeUpper = confirm("Will this include uppercase letters?");
       includeLower = confirm("Will this include lowercase letters?");
-  };
+  }
+
+   // if user did not choose any criteria 
+   if (!includeNum && !includespChars && !includeUpper && !includeLower) {
+      alert("You must choose a criteria. Please click Generate Password again.");
+  }
+
 
 
 }
